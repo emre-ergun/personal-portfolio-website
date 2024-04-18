da5d89e8c357c68590527923c9bf8bbd3c6b6501
Emre Ergun <emreergun@engramsoft.com>
2024-04-18 17:43:01 +0300

    navbar updated, smooth effect added for sections

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import logo from '../assets/minilogo.png';
 
 const NavBar = () => {
   return (
-    <header>
-      <nav className="max-container">NavBar</nav>
+    <header className="mx-4 mt-6 sticky top-6">
+      <nav className="max-container flex justify-between items-center">
+        <div>
+          <a href="#home">
+            <img src={logo} alt="logo" className="w-6 h-6" />
+          </a>
+        </div>
+        <div>
+          <ul className="flex justify-center items-center gap-4 ">
+            <li>
+              <a href="#skill">Skills</a>
+            </li>
+            <li>
+              <a href="#project">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact Me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
